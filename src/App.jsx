afe5e94c325c4da497ded38e0d89671320d27e82
kanishk kashyap <kanishkkashyap840@gmail.com>
2023-04-19 15:59:11 +0530

    Project Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-
+import "./app.css"
 import Home from './Pages/Home/Home'
 import JobDetails from './Pages/JobDetails/JobDetails'
+import { JobPage } from './Pages/JobPage/JobPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <JobDetails/>
+
+    <JobPage/>
   )
 }
 
--- src/Pages/JobPage/JobPage.jsx
+++ src/Pages/JobPage/JobPage.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
-
+import "./jobPage.css"
+import Jobpage_banner from "./jobpage_banner.png"
 export const JobPage = () => {
   return (
     <>
       {/* Banner Section */}
 
+    <div className='jobpage_banner_div'>
+
+    <div>
+    <h1 className='jobpage_banner_heading'>Find Your Dream Job</h1>
+      <small>Looking for Jobs? Browse our latest jobs and apply today!</small>
+    </div>
+      
+      <div className='jobpage_banner_png'>
+        <img className="jobpage_banner_logo" src={Jobpage_banner}/>
+      </div>
+    </div>
       
 
     </>
   )
 }
+
+// How to import Image in React?
