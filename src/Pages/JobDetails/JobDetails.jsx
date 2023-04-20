454d492b8384a5a2788d1803d597ed14149c61b3
Yash2099 <yashasvibaranwal220@gmail.com>
2023-04-20 10:46:11 +0530

    Added the titles

--- src/Pages/JobDetails/JobDetails.jsx
+++ src/Pages/JobDetails/JobDetails.jsx
@@ -5,7 +5,14 @@ import "./JobDetails.css"
 const JobDetails = () => {
   return (
     <>
-      <button>Click Here</button>
+      <title>Job Details</title>
+      <div class="top">
+        <header>
+          <h1 class="detail_title" title="Company Role">Role in Company</h1>
+        </header>
+        <div class="company_name">,<a href="" title="Company Name">Company Name</a>
+        </div>
+      </div>
 
 
     </>
