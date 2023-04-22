16703d921014d0ae51771ea38092473cca38f888
Yash2099 <yashasvibaranwal220@gmail.com>
2023-04-22 08:38:37 +0530

    Added some headings

--- src/Pages/JobDetails/JobDetails.jsx
+++ src/Pages/JobDetails/JobDetails.jsx
@@ -13,6 +13,21 @@ const JobDetails = () => {
         <div class="company_name">,<a href="" title="Company Name">Company Name</a>
         </div>
       </div>
+      <div class="detail view">
+        <div class="internship">
+          <div class="active_hiring">
+            <span>Get hired as quickly as possible</span>
+          </div>
+          <div class="individual_internship_header">
+            <div class="company">
+              <h3 class="job_role_heading">
+                <span class="profile-detail_page">Job Role to be specified</span>
+              </h3>
+              <h4 class="heading_company_name"></h4>
+            </div>
+          </div>
+        </div>
+      </div>
 
 
     </>
