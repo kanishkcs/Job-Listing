df2a4fe873dca0c376ceafe48708358dd8d1d44c
Akriti kumari <akritiom2003@gmail.com>
2023-04-25 11:23:25 +0530

    Removed Overflow From Body

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,84 @@
 import React from 'react'
-
+import "./home.css"
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+        <div className="container">
+        <div className="item brandName">
+            <h1> <a href="#"> First Step</a></h1>
+        </div>
+        <div className="middle_container content">
+            <a href="">Find Jobs</a>
+            <a href="">Find Talent</a>
+            <a href="">Upload Job</a>
+            <a href="">About us</a>
+        </div>
+        <div className="right_container icon">
+            <img className="bell_icon" src="bell.png" />
+            <a href="#">Search</a>
+           
+          
+        </div>
+    </div>
+    <div className="box">
+        <div className="box1 box1Text">
+            <h1> Find the most exciting jobs.</h1>
+            <p>Leverage agile framworks to provide a robust synopsis for high level overviews.iterative to 
+            </p>
+        </div>
+        <div className="box1 box1Image">
+            <img src=" https://i0.wp.com/gbsn.org/wp-content/uploads/2022/01/gbsn-homepage-graphic-1.png?resize=768%2C768&ssl=1"alt="" width="400" height="300" />
+        </div>
+    </div>
+    <div className="container2">
+        <h7>Get hired in top companies</h7>
+
+    </div>
+    <div className="container3">
+        <div className="content3" >
+          
+                <span> Makeless</span>
+                <span>coworks</span>
+                <span>greener</span>
+                <span>SAAS TODAY</span>
+                <span> dorfus</span>
+           
+        </div>
+        
+    </div>
+    <div className="container4">
+        <h1> Explore by category</h1>
+     </div>   
+        
+    <div>
+        <p> Leverage agile frameworks to provide a robust synopsis for high level overview to start.</p>
+    </div>
+    <div  className="contaner5">
+        <div className=" item content">
+            <u1>
+                <li> web development</li>
+                <li>Development</li>
+                <li>Data mimng</li>
+                <li>Data Information </li>
+                <li>Moblie Development</li>
+                <li>App Design</li>
+                
+            </u1>
+        </div> 
+    </div>
+    <div className="container6">
+        <div className="container text">
+            <h3> Most comprehensive job portal</h3>
+            <p> we must explain to you how all this mistake idea of denouncing</p>
+
+        </div>
+        <div className="container info">
+           <a href="#">LON IN </a>
+           <a href="#">REGISTER</a>
+        </div>
+
+    </div>
+    </>
   )
 }
 
