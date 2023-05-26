843aec49f1f2747cc4d47e89ddb49c57c13bd079
VedantSharma11 <vedants2003@gmail.com>
2023-05-26 20:09:29 +0530

    job info page completed without props

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter as Router , Routes, Route } from "react-router-dom";
 import { JobPage } from './Pages/JobPage/JobPage';
-import JobDetails from './Pages/JobDetails/JobDetails';
 import Home from './Pages/Home/Home';
 import Navbar from './Component/Navbar';
 ReactDOM.createRoot(document.getElementById('root')).render(
